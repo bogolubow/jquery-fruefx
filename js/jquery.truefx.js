1c0802d97fdba2cb47dad72eafe8603478243978
Mateusz Bogolubow <mateusz@bogolubow.com>
2017-07-19 11:08:09 +0200

    error Access-Control-Allow-Origin

--- js/jquery.truefx.js
+++ js/jquery.truefx.js
@@ -1,3 +1,56 @@
-jQuery(function(){
-    console.log('DOM!');
+(function( $ ) {
+
+    function getData(url, callback) {
+        var $ajax = $.ajax({
+            url: url
+        });
+
+        $ajax.done(function(resp){
+
+
+            callback(null, resp);
+        });
+
+        $ajax.fail(function(err){
+            callback(err);
+        });
+    }
+
+    $.fn.TrueFx = function( options ) {
+        var opts = $.extend( {}, $.fn.TrueFx.defaults, options );
+
+        var trueFx = {
+            id: null,
+            start: function() {
+                console.log('starting', opts);
+
+                if(this.id === null) {
+                    this.id = setInterval(function(){
+                        getData(opts.url+'?c='+opts.pair+'&f=csv', opts.callback);
+                    }, opts.interval);
+                }
+            },
+
+            stop: function() {
+                console.log('stoping');
+            }
+        }
+
+        return trueFx;
+    }
+})( jQuery );
+
+$.fn.TrueFx.defaults = {
+    interval: 1000,
+    pair: "EUR/USD",
+    url: "http://webrates.truefx.com/rates/connect.html"
+};
+
+
+var trueFx = $.fn.TrueFx({
+    callback: function(err, resp) {
+        console.log(err, resp);
+    }
 });
+console.log(trueFx);
+trueFx.start();
